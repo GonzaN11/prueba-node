d29da483f453e4a8a0e40577b4ad6580c6053535
Gonza Navas <gnavas@focasoftware.com>
2023-03-29 12:21:18 -0300

    Finalizado proyecto base con comunicacion con DB sql server

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # prueba-node
 Una prueba basica de un proyecto de nodejs
+
+# instalacion
+Correr dentro de la ubicacion del proyecto el comando 'npm install' para instalar las dependencias
+
+Para probar la comunicacion correr dentro del proyecto 'nodemon index.js' para darle inicio a la API
+Luego en POSTMAN o en el Navegador correr la ruta localhost:17500/prueba-node/test para prueba de comunicación con DB
--- bll/Test.js
+++ bll/Test.js
@@ -3,7 +3,18 @@ module.exports = {
         return new Promise(async (resolve, reject) => {
             console.log('Test de comunicacion');
             try {
-                resolve('Hay comunicacion')
+                await knex.raw(`SELECT NOM FROM APAREMP WHERE ZON = 1`)
+                .then((resp) => {
+                    if(resp.length > 0){                        
+                        resolve(`Hay comunicacion con la empresa: ${resp[0].NOM}`)
+                    }  else {                        
+                        resolve(`Hay comunicacion pero no hay empresa`)
+                    }
+                })
+                .catch(err => {
+                    console.log(`HUBO UN ERROR AL BUSCAR EMPRESA EN LA APAREMP `, err)
+                    reject(err);
+                })
             } catch (err) {
                 console.log('error catch no enviado ' + err);
                 reject(err)
